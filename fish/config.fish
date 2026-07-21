b092e8ed508ab1b100aaeda5ccfdc5de53cf0f32
sagewood-theme <angad.sigma420@gmail.com>
2026-07-21 21:21:56 +1000

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+# --- Sagewood Theme Colors ---
+    set -g fish_color_normal d2ded7
+    set -g fish_color_command a8b59b
+    set -g fish_color_keyword caacc2
+    set -g fish_color_quote d6caa3
+    set -g fish_color_redirection 9bb5b1
+    set -g fish_color_end d99c82
+    set -g fish_color_error d99c82
+    set -g fish_color_param 8da7b3
+    set -g fish_color_comment 677a70
+    set -g fish_color_option a8b59b
+
+    # Selection & UI
+    set -g fish_color_selection --background=3b4a43 d2ded7
+    set -g fish_color_search_match --background=3b4a43 d2ded7
+    set -g fish_color_operator 9bb5b1
+    set -g fish_color_escape 9bb5b1
+    set -g fish_color_autosuggestion 677a70
+    set -g fish_color_cancel d99c82
+
+    # Completion Menu
+    set -g fish_pager_color_completion d2ded7
+    set -g fish_pager_color_description 677a70
+    set -g fish_pager_color_prefix a8b59b --bold
+    set -g fish_pager_color_progress 9bb5b1
+    set -g fish_pager_color_selected_background --background=3b4a43
+end
